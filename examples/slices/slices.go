d6715b4ce6803fb3328c4c8394cade9841b073c8
hhow09 <hhow09@gmail.com>
2023-02-18 18:56:06 +0800

    feat: update slices (#10)

--- examples/slices/slices.go
+++ examples/slices/slices.go
@@ -41,18 +41,18 @@ func main() {
 	fmt.Println("b1", b1, "cap", cap(b1), "len", len(b1))
 
 	// 5. copy slice with copy (new memory allocation)
-	s1 = []int{1, 2, 3}
-	b2 := make([]int, len(s1))
+	s3 := []int{1, 2, 3}
+	b3 := make([]int, len(s3))
 	// same as "copy" func see doc at: go doc builtin.copy
-	copy(b2, s1)
-	fmt.Printf("copy %v to new array %v\n", s1, b2)
+	copy(b3, s3)
+	fmt.Printf("copy %v to new array %v\n", s3, b3)
 	// or inline copy with append
 	fmt.Printf("inline copy %v with append: %v\n",
-		s1, append([]int{}, s1...))
+		s3, append([]int{}, s3...))
 	// will not be affected by modifying s1
-	fmt.Println("b2", b2, "cap", cap(b2), "len", len(b2))
-	s1[2] = 41
-	fmt.Println("b2", b2, "cap", cap(b2), "len", len(b2))
+	fmt.Println("b3", b3, "cap", cap(b3), "len", len(b3))
+	s3[2] = 41
+	fmt.Println("b3", b3, "cap", cap(b3), "len", len(b3))
 
 	// 6. convert static array to slice
 	key := [16]byte{}
@@ -61,20 +61,21 @@ func main() {
 		cap(keyS), "len", len(keyS))
 
 	// 7. increase the length of slice
-	s2 = make([]int, 3, 10)
-	fmt.Println("s2", s2, "cap", cap(s2), "len", len(s2))
+	s4 := make([]int, 3, 10)
+	fmt.Println("s4", s4, "cap", cap(s4), "len", len(s4))
 	// increase the length of slice with slicing operator
-	// works up until the cap(s2)
-	s2 = s2[:5]
-	fmt.Println("s2", s2, "cap", cap(s2), "len", len(s2))
-	s1 = []int{1, 2, 3}
-	fmt.Println("s1", s1, "cap", cap(s1), "len", len(s1))
-	s1 = appendInt(s1, 31)
-	fmt.Println("s1", s1, "cap", cap(s1), "len", len(s1))
+	// works up until the cap(s4)
+	s4 = s4[:5] // len=3, cap=5
+	fmt.Println("s4", s4, "cap", cap(s4), "len", len(s4))
+	s5 := []int{1, 2, 3}
+	fmt.Println("s5", s5, "cap", cap(s5), "len", len(s5))
+	// trigger the capacity increase since reached the threshold
+	s5 = append_(s5, 31) // cap 3 -> 8, len 3 -> 4
+	fmt.Println("s5", s5, "cap", cap(s5), "len", len(s5))
 
 }
 
-func copyInt(dst, src []int) int {
+func copySlice[K any](dst, src []K) int {
 	var n int
 	for i, _ := range src {
 		dst[i] = src[i]
@@ -83,14 +84,15 @@ func copyInt(dst, src []int) int {
 	return n
 }
 
-func appendInt(s []int, e int) []int {
+// simulate the append of go internal
+func append_[K any](s []K, e K) []K {
 	expLen := len(s) + 1
 	if cap(s) < expLen {
 		// grow the slice
 		// new allocation create new slice from scratch
-		newS := make([]int, expLen, (cap(s)+1)*2)
+		newS := make([]K, expLen, (cap(s)+1)*2)
 		// copy the data from old to new
-		copyInt(newS, s)
+		copySlice(newS, s)
 		// assign new to old
 		s = newS
 	}
--- examples/slices/slices.sh
+++ examples/slices/slices.sh
@@ -11,13 +11,13 @@ b1 [2 3] cap 2 len 2
 b1 [2 42] cap 2 len 2
 copy [1 2 3] to new array [1 2 3]
 inline copy [1 2 3] with append: [1 2 3]
-b2 [1 2 3] cap 3 len 3
-b2 [1 2 3] cap 3 len 3
+b3 [1 2 3] cap 3 len 3
+b3 [1 2 3] cap 3 len 3
 keyS [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] cap 16 len 16
-s2 [0 0 0] cap 10 len 3
-s2 [0 0 0 0 0] cap 10 len 5
-s1 [1 2 3] cap 3 len 3
-s1 [1 2 3 31] cap 8 len 4
+s4 [0 0 0] cap 10 len 3
+s4 [0 0 0 0 0] cap 10 len 5
+s5 [1 2 3] cap 3 len 3
+s5 [1 2 3 31] cap 8 len 4
 
 # Check out this [great blog post](https://go.dev/blog/slices-intro)
 # by the Go team for more details on the design and
