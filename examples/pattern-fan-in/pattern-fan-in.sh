5a8493ea7da87c4f49885835f10fbd814d324442
hhow09 <hhow09@gmail.com>
2023-02-19 22:16:40 +0800

    feat: pattern: fan-in (#13)

---
+++ examples/pattern-fan-in/pattern-fan-in.sh
@@ -0,0 +1,19 @@
+$ go run pattern-fan-in.go
+
+[LOG] receive the merged channel.
+[LOG] Wait for the exit signal.
+[CSV Result] [file2 make large research] 
+[CSV Result] [file2 stand skill solve] 
+[CSV Result] [file1 rhythm proper secret] 
+[CSV Result] [file1 scene screen bright] 
+[CSV Result] [file2 you sometime worth] 
+[CSV Result] [file2 end uncle long] 
+[CSV Result] [file2 ate adjective protection] 
+[CSV Result] [file2 nothing there bright] 
+[CSV Result] [file1 universe official primitive] 
+[CSV Result] [file1 might worried acres] 
+[CSV Result] [file1 flow slope eye] 
+[CSV Result] [file1 across again threw] 
+[CSV Result] [file1 rich won women] 
+[LOG] merged channel closed.
+All completed, Exit.
